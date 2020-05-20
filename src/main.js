2f7680ba1ee7faaf3ec8aed455a425e47d8cae3d
Jordan <jordan.tanp@gmail.com>
2020-05-20 22:22:46 +0200

    trying to fix deployment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-//import VueRouter from 'vue-router'
+import VueRouter from 'vue-router'
 import VueCookies from 'vue-cookies'
 import App from './App.vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueResource from "vue-resource";
 
-//Vue.use(VueRouter)
+Vue.use(VueRouter)
 Vue.use(VueCookies)
 Vue.$cookies.config('7d')
 Vue.use(BootstrapVue)
@@ -15,6 +15,15 @@ Vue.use(IconsPlugin)
 Vue.use(VueResource);
 Vue.http.options.root = '/api';
 
+new VueRouter({
+    mode: "history",
+    routes: [
+        { path: '/', component: App },
+        { path: '*', redirect: '/'  }
+    ]
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
